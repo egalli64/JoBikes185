1ea925cefc0ff03a88f2f9f0932c367ddb7c2746
Manny <egalli64@gmail.com>
2018-12-21 12:37:12 +0100

    first commit

---
+++ src/main/java/jo/JBApplication.java
@@ -0,0 +1,14 @@
+package jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/jo/JBController.java
@@ -0,0 +1,41 @@
+package jo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class JBController {
+	static final Logger logger = LoggerFactory.getLogger(JBController.class);
+
+	// not a good idea to use GET here
+	@GetMapping("/login")
+	public String nakedLogin( //
+			@RequestParam(name = "user", defaultValue = "Unknown") String user, //
+			@RequestParam(name = "password") String password, //
+			Model model) {
+		logger.warn("Unsafe login attempt for user: " + user);
+
+		model.addAttribute("user", user);
+		return "checkPassword";
+	}
+
+	@PostMapping("/login")
+	public String login( //
+			@RequestParam(name = "user") String user, //
+			@RequestParam(name = "password") String password, //
+			Model model) {
+		logger.debug("Login attempt for user: " + user);
+
+		model.addAttribute("user", user);
+		if (user.equals("manager") && password.equals("")) {
+			return "manager";
+		} else {
+			return "checkPassword";
+		}
+	}
+}
