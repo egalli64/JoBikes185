c896bd929573fd9f469927598e24159bdc60fc0e
Manny <egalli64@gmail.com>
2018-12-29 11:03:37 +0100

    Select countries by region

--- src/main/java/jo/controller/CountryCtrl.java
+++ src/main/java/jo/controller/CountryCtrl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jo.model.CountryRepository;
 
@@ -17,9 +18,21 @@ public class CountryCtrl {
 	CountryRepository repo;
 
 	@GetMapping("/countries")
-	public String showCountries(Model model) {
-		logger.debug("Show countries");
+	public String allCountries(Model model) {
+		logger.debug("All countries");
 		model.addAttribute("countries", repo.findAll());
 		return "countries";
+	}
+
+	@GetMapping("/countriesByRegion")
+	public String countriesByRegion( //
+			@RequestParam Integer id, //
+			@RequestParam String name, //
+			Model model) {
+		logger.debug(String.format("Countries by region %d (%s)", id, name));
+
+		model.addAttribute("countries", repo.findByRegId(id));
+		model.addAttribute("region", name);
+		return "countriesByRegion";
 	}
 }
