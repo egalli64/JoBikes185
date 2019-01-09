85913ab005fff43a81a5c0570a2bc34f75a21745
suannacanevaro <susanna.canevaro@gmail.com>
2019-01-09 17:19:38 +0100

    CLEAN UP

--- src/main/java/jo/model/entities/Station.java
+++ src/main/java/jo/model/entities/Station.java
@@ -12,57 +12,54 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-	@Entity
-	@Table(name = "STATIONS")
-	public class Station {
-		@Id
-		@Column(name = "STATION_ID")
-		private Integer id;
+@Entity
+@Table(name = "STATIONS")
+public class Station {
+	@Id
+	@Column(name = "STATION_ID")
+	private Integer id;
 
-		@Column(name = "STATION_NAME")
-		private String name;
+	@Column(name = "STATION_NAME")
+	private String name;
 
-		@Column(name = "CITY_ID")
-		private Integer cityId;
+	@Column(name = "CITY_ID")
+	private Integer cityId;
 
-		public Station() {
-		}
-
-		public Station(Integer id, String name, Integer cityId) {
-			this.id = id;
-			this.name = name;
-			this.cityId = cityId;
-		}
-
-		public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public Integer getCityId() {
-			return cityId;
-		}
-
-		public void setCityId(Integer cityId) {
-			this.cityId = cityId;
-		}
-
-		@Override
-		public String toString() {
-			return "Station [id=" + id + ", name=" + name + ", cityId=" + cityId + "]";
-		}
-
-		
+	public Station() {
 	}
 
+	public Station(Integer id, String name, Integer cityId) {
+		this.id = id;
+		this.name = name;
+		this.cityId = cityId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCityId() {
+		return cityId;
+	}
+
+	public void setCityId(Integer cityId) {
+		this.cityId = cityId;
+	}
+
+	@Override
+	public String toString() {
+		return "Station [id=" + id + ", name=" + name + ", cityId=" + cityId + "]";
+	}
+}
