42369bef8171e2931a30141366f397ec2c166f25
Administrator <Administrator@192.168.12.104>
2019-01-09 09:33:09 +0100

    First Commit

---
+++ src/main/java/jo/model/CityRepository.java
@@ -0,0 +1,22 @@
+/**
+ * (c) 2019 ACME Inc.
+ * JPA repository for Cities
+ * 
+ * @author Ariam
+ */
+package jo.model;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jo.model.entities.City;
+import jo.model.entities.Region;
+
+@Repository
+public interface CityRepository extends CrudRepository<City,Integer> {
+	
+	
+
+}
